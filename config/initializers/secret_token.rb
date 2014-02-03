58a1db74927264cb2671dcfa2a6fcf6213683868
Jordan Morano <jordan.morano@gmail.com>
2014-02-03 14:51:17 -0500

    Initial app setup with Gemfile updated to include dev gems and devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FriendFitter::Application.config.secret_key_base = 'fb18bcea0aec1dc050df23617c0d6742d4666c3b965adf6fd6e21537f18b794f88c8c4ff08c5822f6e6d222c0694bec031079d9a961c77bca65aae911abd4efe'
