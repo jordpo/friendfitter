152402f91e14c407ffcbc682b333282546f8c80e
Jordan Morano <jordan.morano@gmail.com>
2014-02-03 20:25:41 -0500

    Workout and Exercise Controller initial setup, worked on workout#index - basic layout complete

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,29 @@
 class ExercisesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:name, :detail, :duration, :started)
+  end
 end
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,30 @@
 class WorkoutsController < ApplicationController
+  def index
+    @workouts = Workout.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:name, :difficulty, :posted, :started)
+  end
 end
