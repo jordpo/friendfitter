85a1e8a724841a3e731a42b3b9d4209d07935fa2
Jordan Morano <jordan.morano@gmail.com>
2014-02-06 14:12:47 -0500

    Workout passes all validation tests

--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -20,8 +20,20 @@ describe Workout do
     workout = FactoryGirl.create(:workout)
     expect(workout).to be_valid
   end
-  it 'is invalid without a name'
-  it 'is invalid without a difficulty'
-  it 'is invalid without a user_id'
-  it 'is invalid without a community_id'
+  it 'is invalid without a name' do
+    workout = FactoryGirl.build(:workout, name: nil)
+    expect(workout).to_not be_valid
+  end
+  it 'is invalid without a difficulty' do
+    workout = FactoryGirl.build(:workout, difficulty: nil)
+    expect(workout).to_not be_valid
+  end
+  it 'is invalid without a user_id' do
+    workout = FactoryGirl.build(:workout, user_id: nil)
+    expect(workout).to_not be_valid
+  end
+  it 'is invalid without a community_id' do
+    workout = FactoryGirl.build(:workout, community_id: nil)
+    expect(workout).to_not be_valid
+  end
 end
