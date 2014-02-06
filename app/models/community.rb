1c4cea83db6469a76425f24d63f6c32d1b7aadb7
Jordan Morano <jordan.morano@gmail.com>
2014-02-06 13:49:13 -0500

    Community model passes validation tests

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,4 +1,6 @@
 class Community < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_many :workouts
+  validates :name, presence: true
+  validates :description, presence: true, length: { maximum: 160 }
 end
--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -4,8 +4,22 @@ require 'spec_helper'
 
 describe Community do
   it 'has a valid factory' do
-    FactoryGirl.create(:community).should be_valid
+    community = FactoryGirl.create(:community)
+    expect(community).to be_valid
   end
-  it 'is invalid without a name'
-  it 'is invalid without description'
+  it 'is invalid without a name' do
+    community = FactoryGirl.build(:community, name: nil)
+    expect(community).to_not be_valid
+  end
+  it 'is invalid without description' do
+    community = FactoryGirl.build(:community, description: nil)
+    expect(community).to_not be_valid
+  end
+  it 'is invalid with a description greater than 160 characters' do
+    description = 'jkafdkanfandlfnajkdsnfkljandjfnakjdnfkajdnfjknadjfnajlkjanfkjdn
+      adnjkjandflkandlkjfnadkljnfalkjndfljkandsfkjandkjfnakdsjnfjkandlfkjandjkn
+      akfdjnaldkakjnfdkjandjkfnakjdnfakjldnfljkandljkfnad'
+    community = FactoryGirl.build(:community, description: description)
+    expect(community).to_not be_valid
+  end
 end
