17d6f0de0cd64125887bb4e57068ac7ee56a668d
Jordan Morano <jordan.morano@gmail.com>
2014-02-06 14:02:46 -0500

    all tests pass for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,12 @@ describe User do
     user = FactoryGirl.create(:user)
     expect(user).to be_valid
   end
-  it 'is invalid without a username'
-  it 'is invalid without an email'
+  it 'is invalid without a username' do
+    user = FactoryGirl.build(:user, username: nil)
+    expect(user).to_not be_valid
+  end
+  it 'is invalid without an email' do
+    user = FactoryGirl.build(:user, email: nil)
+    expect(user).to_not be_valid
+  end
 end
